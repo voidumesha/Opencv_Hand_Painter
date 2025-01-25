54d9acbfe3fa9f702f7348421069bb1acea7a752
Umesha Rukshan Silva <umesharukshan2000@gmail.com>
2025-01-25 22:37:51 +0530

    Initial commit

---
+++ VirtualPanter.py
@@ -0,0 +1,84 @@
+import cv2
+import numpy as np
+import time
+import os
+import HandTrackingModule as htm
+
+brushThickness = 12
+eraserThickness = 120
+
+folderPath = "Header"
+myList = os.listdir(folderPath)
+print(myList)
+overLayList = []
+for imPath in myList:
+    image = cv2.imread(f'{folderPath}/{imPath}')
+    overLayList.append(image)
+print(len(overLayList))
+header = overLayList[0]
+
+drawColor =  (225, 0, 225)
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 1280)
+cap.set(4, 720)
+
+detector = htm.handDetector(detectionCon=0.85)
+xp, yp = 0, 0
+
+imgCanvas = np.zeros((720, 1280, 3), np.uint8)
+
+while True:
+    success, img = cap.read()
+    img = cv2.flip(img, 1)
+
+    img = detector.findHands(img)
+    lmList, bbox = detector.findPosition(img, draw=False)
+
+    if len(lmList) != 0:
+        x1, y1 = lmList[8][1:]
+        x2, y2 = lmList[12][1:]
+
+        fingers = detector.fingersUp()
+
+        if fingers[1] and fingers[2]:
+            xp, yp = 0, 0
+            if y1 < 125:
+                if 250 < x1 < 450:
+                    header = overLayList[0]
+                    drawColor = (225, 0, 225)
+                elif 550 < x1 < 750:
+                    header = overLayList[1]
+                    drawColor = (225, 0, 0)
+                elif 800 < x1 < 950:
+                    header = overLayList[2]
+                    drawColor = (0, 225, 0)
+                elif 1050 < x1 < 1200:
+                    header = overLayList[3]
+                    drawColor = (0, 0, 0)
+            cv2.rectangle(img, (x1, y1 - 25), (x2, y2 + 25), drawColor, cv2.FILLED)
+
+        if fingers[1] and fingers[2] == False:
+            cv2.circle(img, (x1, y1), 15, drawColor, cv2.FILLED)
+            if xp == 0 and yp == 0:
+                xp, yp = x1, y1
+
+            if drawColor == (0, 0, 0):
+                cv2.line(img, (xp, yp), (x1, y1), drawColor, eraserThickness)
+                cv2.line(imgCanvas, (xp, yp), (x1, y1), drawColor, eraserThickness)
+            else:
+                cv2.line(img, (xp, yp), (x1, y1), drawColor, brushThickness)
+                cv2.line(imgCanvas, (xp, yp), (x1, y1), drawColor, brushThickness)
+
+            xp, yp = x1, y1
+
+    imgGray = cv2.cvtColor(imgCanvas, cv2.COLOR_BGR2GRAY)
+    _, imgInv = cv2.threshold(imgGray, 50, 225, cv2.THRESH_BINARY_INV)
+    imgInv = cv2.cvtColor(imgInv, cv2.COLOR_GRAY2BGR)
+    img = cv2.bitwise_and(img, imgInv)
+    img = cv2.bitwise_or(img, imgCanvas)
+
+    img[0:125, 0:1280] = header
+    img = cv2.addWeighted(img, (1), imgCanvas, 0.5, 0)
+    cv2.imshow("image", img)
+    cv2.waitKey(1)
